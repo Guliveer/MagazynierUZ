a3fd577f9403d384ef7f216336c96ab5b43d580a
Dawid Naraziński <dawidelel@gmail.com>
2025-10-22 22:53:00 +0200

    #3 initialize spring project, and connect database via supabase

---
+++ magazynieruz/src/test/java/org/example/magazynieruz/MagazynieruzApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.magazynieruz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagazynieruzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
