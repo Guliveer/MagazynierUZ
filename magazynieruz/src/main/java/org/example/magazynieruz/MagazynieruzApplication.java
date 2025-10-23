0fcdee6d178aca6b7502e4b6a2640a0c1fb9f673
Dawid Naraziński <65094032+DawSkii@users.noreply.github.com>
2025-10-23 20:59:31 +0200

    Dawskii/3 initialize spring boot project (#8)

---
+++ magazynieruz/src/main/java/org/example/magazynieruz/MagazynieruzApplication.java
@@ -0,0 +1,13 @@
+package org.example.magazynieruz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagazynieruzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagazynieruzApplication.class, args);
+    }
+
+}
