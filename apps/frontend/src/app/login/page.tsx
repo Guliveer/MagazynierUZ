c7c68e297c8fc7bef4aba446c5e736b80e4fcdb9
zytasarniak <zyta.sarniak@gmail.com>
2025-11-11 11:48:02 +0100

    import change to shadcn
    Co-authored-by: Oliwer Pawelski <oliwer.pawelski@gmail.com>

--- apps/frontend/src/app/login/page.tsx
+++ apps/frontend/src/app/login/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 import { useState } from "react";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { Button } from "shadcn/button";
+import { Input } from "shadcn/input";
+import { Label } from "shadcn/label";
 import {
   Card,
   CardHeader,
@@ -10,7 +10,7 @@ import {
   CardDescription,
   CardContent,
   CardFooter,
-} from "@/components/ui/card";
+} from "shadcn/card";
 import Link from "next/link";
 
 export default function LoginPage() {
