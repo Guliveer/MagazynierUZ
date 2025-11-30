287f8fa61494466b6dc4723ea8b1e86d36fdbcb0
Oliwer Pawelski <oliwer.pawelski@gmail.com>
2025-11-30 17:22:59 +0100

    Add ESLint (#28)

--- apps/frontend/src/app/themeProvider.tsx
+++ apps/frontend/src/app/themeProvider.tsx
@@ -1,11 +1,11 @@
-"use client";
-import { ThemeProvider } from "next-themes";
-import React from "react";
+'use client';
+import { ThemeProvider } from 'next-themes';
+import React from 'react';
 
 export default function ThemeProviderWrapper({children}: { children: React.ReactNode }) {
     return (
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange
-                       storageKey="theme">
+            storageKey="theme">
             {children}
         </ThemeProvider>
     );
